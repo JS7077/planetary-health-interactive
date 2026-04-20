22da31f0de5da7416c15bf94d6f7358f5eb10b0e
JS7077 <139284744+JS7077@users.noreply.github.com>
2026-04-20 08:24:46 -0700

    positioned and textured earth

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,14 @@ import { Canvas } from '@react-three/fiber'
 import './App.css'
 import { Earth } from './Earth.tsx'
 
-function Box() {
-  return (
-  <mesh>
-    <boxGeometry args={[1,2,3]}/>
-    <meshBasicMaterial color={'green'}/>
-  </mesh>
-)
-}
-
-
-
-
 function App() {
 
   return (
     <div id='canvasDiv'>
-      <Canvas camera={{ position: [-20, 0, -20], fov: 50 }}>
-        <Box />
-        <Earth position={[1, -1, 1]}/>
+      <Canvas camera={{ position: [0, 0, -30], fov: 50 }}>
+        <ambientLight intensity={1.5} /> 
+        <directionalLight position={[10, 10, 5]} intensity={2} />
+        <Earth position={[10, -11, 1]} scale={1.2} />
       </Canvas>
     </div>
   )
