7544c6478100d64da20c6de90af5ea45d2dcbe78
JS7077 <139284744+JS7077@users.noreply.github.com>
2026-04-20 09:20:29 -0700

    making a face on the earth

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Canvas camera={{ position: [0, 0, -30], fov: 50 }}>
         <ambientLight intensity={1.5} /> 
         <directionalLight position={[10, 10, 5]} intensity={2} />
+
         <Earth position={[10, -11, 1]} scale={1.2} />
       </Canvas>
     </div>
